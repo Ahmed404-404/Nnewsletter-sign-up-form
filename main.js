551829feea99c9b5602c081d27c7a85cb93e829e
Ahmed Shband <60365776+Ahmed404-404@users.noreply.github.com>
2023-06-01 02:06:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+let email = document.querySelector("#email")
+let submitBtn = document.querySelector("#submit")
+let dissmissBtn = document.querySelector("#dissmiss")
+let label = document.querySelector("label")
+let seccessSec = document.querySelector(".seccess")
+let signSec = document.querySelector(".sign-up")
+let reg = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+let state = null
+
+email.value = ""
+email.addEventListener("blur",()=>{
+    validate()
+})
+submitBtn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    validate()
+    if(state){
+        signSec.classList.add("hide")
+        seccessSec.classList.remove("hide")
+        state = false
+    }
+
+})
+dissmissBtn.addEventListener("click",()=>{
+    seccessSec.classList.add("hide")
+    signSec.classList.remove("hide")
+    email.value = ""
+})
+
+
+function validate(){
+    if(email.value !== ""){
+        if(reg.test(email.value)){
+            console.log("true")
+            email.classList.remove("not-valid")
+            label.removeAttribute("data-valid")
+            state = true
+        }else{
+            email.classList.add("not-valid")
+            label.setAttribute("data-valid","valid email required")
+            state = false
+            console.log("false")
+        }
+    }
+}
